239c4696271f8c071ec885314dc3eedfde00b239
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-21 21:47:05 +0200

    Implement basic player navigation

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+class_name Player
+
+export var movement_speed: int = 100
+
+export var navigation: NodePath
+onready var _navigation: Navigation2D = get_node(navigation)
+
+var _velocity := Vector2.ZERO
+
+func _ready() -> void:
+	PlayerNavigationTarget.connect("new_player_navigation_target", self, "_on_new_player_target")
+
+
+func _physics_process(delta: float) -> void:
+	if $NavigationAgent2D.is_navigation_finished(): return
+	
+	var direction := global_position.direction_to($NavigationAgent2D.get_next_location())
+	
+	var desired_velocity := direction * movement_speed
+	var steering := (desired_velocity - _velocity) * delta * 4.0
+	_velocity += steering
+	_velocity = move_and_slide(_velocity)
+
+func _on_new_player_target(new_target: Vector2) -> void:
+	$NavigationAgent2D.set_target_location(new_target)
+
+
+func _on_NavigationAgent2D_navigation_finished() -> void:
+	print("Player navigation finished")
+	PlayerNavigationTarget.emit_signal("player_navigation_finished")
---
+++ singletons/PlayerNavigationTarget.gd
@@ -0,0 +1,7 @@
+extends Node
+# set through autoload
+#class_name PlayerNavigationTarget
+
+
+signal new_player_navigation_target(target)
+signal player_navigation_finished
