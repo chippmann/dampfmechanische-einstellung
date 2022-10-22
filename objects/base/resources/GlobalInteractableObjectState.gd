376df51d82bd39e47c188428b0f622f55fcd4636
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-22 11:00:33 +0200

    Remove now unneeded action global state

--- objects/base/resources/GlobalInteractableObjectState.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name GlobalInteractableObjectState
-
-var currently_running_action: Resource
-var currently_running_action_is_in_walking_state: bool = false
-
-func _can_execute_action() -> bool:
-	return currently_running_action == null || currently_running_action_is_in_walking_state == true
