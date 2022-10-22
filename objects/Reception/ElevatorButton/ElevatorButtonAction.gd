7fbc3616e1175e8127d1ee68759f59a10a24d828
Josias Bruderer <josias@macherstube.ch>
2022-10-22 09:45:45 +0200

    fix elevator button bug

--- objects/Reception/ElevatorButton/ElevatorButtonAction.gd
+++ objects/Reception/ElevatorButton/ElevatorButtonAction.gd
@@ -4,5 +4,5 @@ class_name ElevatorButtonAction
 
 func _execute(action_name: String) -> void:
 	GameState.receptionElevatorButtonPressed = true
-	if Inventory.has_object("ScrewDriver"):
+	if Inventory.has_object("ScrewDriver") and not GameState.receptionElevatorOpen:
 		ActionRelay.trigger("ElevatorDoor", "emit_signal", ["pressed"])
