af9ca8b2868ea3252713d9839ca8eec26da0e45b
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-22 15:50:44 +0200

    Remove unused expressions option in action

--- objects/base/resources/ObjectAction.gd
+++ objects/base/resources/ObjectAction.gd
@@ -3,10 +3,8 @@ extends Resource
 export var name: String
 export var audio: AudioStream
 
-export var conditionExpression: String
 export var conditionScript: Script
 
-export var actionExpression: String
 export var actionScript: Script
 export var actionWaitsForAudio: bool = false
 export var playerWalkBeforeAction: bool = true
@@ -14,10 +12,6 @@ export var playerWalkBeforeAction: bool = true
 
 func is_condition_met(action_name: String) -> bool:
 	var condition_expression_result := true
-	if conditionExpression:
-		var expression := Expression.new()
-		assert(expression.parse(conditionExpression) == OK, "Could not parse expression")
-		condition_expression_result = expression.execute([], self)
 	
 	var condition_script_result := true
 	if conditionScript:
@@ -27,11 +21,6 @@ func is_condition_met(action_name: String) -> bool:
 
 
 func execute_action(action_name: String) -> void:
-	if actionExpression:
-		var expression := Expression.new()
-		assert(expression.parse(actionExpression) == OK, "Could not parse expression")
-		expression.execute()
-	
 	if actionScript:
 		actionScript.new()._execute(action_name)
 
