617832cc771261d6ac160ea30326bdf79f464d8f
Josias Bruderer <josias@macherstube.ch>
2022-10-23 00:45:49 +0200

    implement intercom part 1

---
+++ objects/ElevatorMain/Intercom/IntercomHandle/IntercomHandleAction.gd
@@ -0,0 +1,17 @@
+extends Reference
+class_name IntercomHandleAction
+
+
+func _execute(_action_name: String) -> void:
+	if GameState.save_game.act01_screwdriver_in_fuse:
+		ActionRelay.trigger("IntercomInactive", "set_visible", [false])
+		ActionRelay.trigger("IntercomConnecting", "set_visible", [false])
+	else:
+		ActionRelay.trigger("IntercomInactive", "set_visible", [false])
+		#WIP: Timer 3s
+		ActionRelay.trigger("IntercomInactive", "set_visible", [true])
+		
+
+
+func _restore_state(_interactable_object_instance: InteractableObject, _action_name: String) -> void:
+	pass
