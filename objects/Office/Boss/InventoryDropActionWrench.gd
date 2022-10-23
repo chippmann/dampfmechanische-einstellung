bcc83b179581b0a90a6b97a64c8cb2b15b629208
Josias Bruderer <josias@macherstube.ch>
2022-10-23 15:27:56 +0200

    style credits

--- objects/Office/Boss/InventoryDropActionWrench.gd
+++ objects/Office/Boss/InventoryDropActionWrench.gd
@@ -4,3 +4,4 @@ class_name WrenchInventoryDropAction
 
 func _on_inventory_drop(_action_name: String, inventory_id: String, inventory_data: Dictionary) -> void:
 	Inventory.use_item("Wrench")
+	GameState.change_scene_to(7)
