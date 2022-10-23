8baa91ad80d6f8c50e15e53457b2438f82ed425b
Josias Bruderer <josias@macherstube.ch>
2022-10-23 13:33:08 +0200

    add separat gears in bedienfeld

--- objects/ElevatorMain/Bedienfeld/BedienfeldButtons/BedienfeldButtons2-29Action.gd
+++ objects/ElevatorMain/Bedienfeld/BedienfeldButtons/BedienfeldButtons2-29Action.gd
@@ -4,14 +4,31 @@ class_name BedienfeldButtons229Action
 
 func _execute(_interactable_object_instance: InteractableObject, _action_name: String) -> void:
 	match(_action_name):
+		"BedienfeldButtons1_first":
+			GameState.save_game.act01_bedienfeld_buttonpressedfirsttime = true
+			ActionRelay.trigger("Buttons_1", "emit_signal", ["pressed"])
+		"BedienfeldButtons1_notworking":
+			pass
+		"BedienfeldButtons1_working":
+			GameState.save_game.act01_current_floor = 1
+			GameState.change_scene_to(1)
+			yield(_interactable_object_instance.get_tree().create_timer(0.5), "timeout")
+			if !GameState.save_game.act01_door_open:
+				ActionRelay.trigger("ElevatorDoorAnimation", "play", ["default", true])
+				GameState.save_game.act01_door_open = true
 		"BedienfeldButtons2-29_first":
 			GameState.save_game.act01_bedienfeld_buttonpressedfirsttime = true
-			ActionRelay.trigger("Buttons_2-29_1", "emit_signal", ["pressed"])
+			ActionRelay.trigger("Buttons_2-29", "emit_signal", ["pressed"])
 		"BedienfeldButtons2-29_notworking":
 			pass
 		"BedienfeldButtons2-29_working":
 			GameState.save_game.act01_current_floor = 2
 			ActionRelay.trigger("OuterRoomImage1", "set_visible", [false])
+			GameState.change_scene_to(1)
+			yield(_interactable_object_instance.get_tree().create_timer(0.5), "timeout")
+			if !GameState.save_game.act01_door_open:
+				ActionRelay.trigger("ElevatorDoorAnimation", "play", ["default", true])
+				GameState.save_game.act01_door_open = true
 		"BedienfeldButtons30_first":
 			GameState.save_game.act01_bedienfeld_buttonpressedfirsttime = true
 			ActionRelay.trigger("Buttons_30", "emit_signal", ["pressed"])
@@ -21,6 +38,11 @@ func _execute(_interactable_object_instance: InteractableObject, _action_name: S
 			GameState.save_game.act01_current_floor = 30
 			ActionRelay.trigger("OuterRoomImage1", "set_visible", [false])
 			ActionRelay.trigger("OuterRoomImage2-29", "set_visible", [false])
+			GameState.change_scene_to(1)
+			yield(_interactable_object_instance.get_tree().create_timer(0.5), "timeout")
+			if !GameState.save_game.act01_door_open:
+				ActionRelay.trigger("ElevatorDoorAnimation", "play", ["default", true])
+				GameState.save_game.act01_door_open = true
 
 
 
--- objects/ElevatorMain/Bedienfeld/BedienfeldButtons/BedienfeldButtons2-29Condition.gd
+++ objects/ElevatorMain/Bedienfeld/BedienfeldButtons/BedienfeldButtons2-29Condition.gd
@@ -4,6 +4,14 @@ class_name BedienfeldButtons229Condition
 
 func _is_condition_met(_action_name: String) -> bool:
 	match(_action_name):
+		"BedienfeldButtons1_first":
+			return !GameState.save_game.act01_bedienfeld_buttonpressedfirsttime
+		"BedienfeldButtons1_notworking":
+			return GameState.save_game.act01_bedienfeld_buttonpressedfirsttime \
+			&& !GameState.save_game.act01_wartungsschacht_functional_pipe_placed
+		"BedienfeldButtons1_working":
+			return GameState.save_game.act01_bedienfeld_buttonpressedfirsttime \
+			&& GameState.save_game.act01_wartungsschacht_functional_pipe_placed
 		"BedienfeldButtons2-29_first":
 			return !GameState.save_game.act01_bedienfeld_buttonpressedfirsttime
 		"BedienfeldButtons2-29_notworking":
