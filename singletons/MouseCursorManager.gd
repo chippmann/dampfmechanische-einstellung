14fe26aaaa3789f91550473e28d77f5208dafe26
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-23 14:52:25 +0200

    Fix cursor in web export

--- singletons/MouseCursorManager.gd
+++ singletons/MouseCursorManager.gd
@@ -7,5 +7,6 @@ var pointing_hand: Texture = load("res://assets/texture/UiElement/cursor_hand.pn
 
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(arrow)
+	Input.set_custom_mouse_cursor(arrow, Input.CURSOR_FORBIDDEN)
 	Input.set_custom_mouse_cursor(pointing_hand, Input.CURSOR_POINTING_HAND, Vector2(21, 4))
 	Input.set_custom_mouse_cursor(pointing_hand, Input.CURSOR_CAN_DROP, Vector2(21, 4))
