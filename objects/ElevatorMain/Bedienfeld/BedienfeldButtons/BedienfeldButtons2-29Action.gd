42ffd44db235c10b25d048d99039663c05cccf18
Josias Bruderer <josias@macherstube.ch>
2022-10-23 10:19:13 +0200

    prepare bedienfeld

---
+++ objects/ElevatorMain/Bedienfeld/BedienfeldButtons/BedienfeldButtons2-29Action.gd
@@ -0,0 +1,10 @@
+extends Reference
+class_name BedienfeldButtons229Action
+
+
+func _execute(_interactable_object_instance: InteractableObject, _action_name: String) -> void:
+	print("Button")
+
+
+func _restore_state(_interactable_object_instance: InteractableObject, _action_name: String) -> void:
+	pass
---
+++ objects/ElevatorMain/Bedienfeld/BedienfeldButtons/BedienfeldButtons2-29Condition.gd
@@ -0,0 +1,7 @@
+extends Reference
+class_name BedienfeldButtons229Condition
+
+
+func _is_condition_met(_action_name: String) -> bool:
+	print("WARNING: no condition defined. Returning true")
+	return true
