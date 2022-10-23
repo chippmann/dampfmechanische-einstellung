9c3339484b0f65ec21afb5ae895fe4e1c24fcbc0
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-23 14:28:33 +0200

    Add custom mouse cursor

---
+++ singletons/MouseCursorManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+var arrow: Texture = load("res://assets/texture/UiElement/cursor.png")
+var pointing_hand: Texture = load("res://assets/texture/UiElement/cursor_hand.png")
+
+
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(arrow)
+	Input.set_custom_mouse_cursor(pointing_hand, Input.CURSOR_POINTING_HAND, Vector2(21, 4))
