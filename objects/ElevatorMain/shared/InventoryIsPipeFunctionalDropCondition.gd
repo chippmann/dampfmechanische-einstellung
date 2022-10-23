d4263e18b7bc7e9c26f0cf4a99d1ad201e30e294
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-23 12:22:48 +0200

    Add inventory reactions for pipes and bedienfeld

---
+++ objects/ElevatorMain/shared/InventoryIsPipeFunctionalDropCondition.gd
@@ -0,0 +1,6 @@
+extends Reference
+class_name InventoryIsPipeFunctionalDropCondition
+
+
+func _can_handle_inventory_drop(_action_name: String, inventory_id: String, inventory_data: Dictionary) -> bool:
+	return inventory_id == "Pipe_Functional"
