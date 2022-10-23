886539a52adb0fcf19069862ba16e6f60b93d4a7
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-23 14:30:36 +0200

    Fix drop cursor

--- singletons/MouseCursorManager.gd
+++ singletons/MouseCursorManager.gd
@@ -8,3 +8,4 @@ var pointing_hand: Texture = load("res://assets/texture/UiElement/cursor_hand.pn
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(arrow)
 	Input.set_custom_mouse_cursor(pointing_hand, Input.CURSOR_POINTING_HAND, Vector2(21, 4))
+	Input.set_custom_mouse_cursor(pointing_hand, Input.CURSOR_CAN_DROP, Vector2(21, 4))
