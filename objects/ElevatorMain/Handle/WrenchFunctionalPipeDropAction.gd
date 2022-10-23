921bf03e00b796b82af4543cc4a5e07f84f22ea6
Josias Bruderer <josias@macherstube.ch>
2022-10-23 14:16:03 +0200

    fix grafix

--- objects/ElevatorMain/Handle/WrenchFunctionalPipeDropAction.gd
+++ objects/ElevatorMain/Handle/WrenchFunctionalPipeDropAction.gd
@@ -3,6 +3,6 @@ class_name WrenchFunctionalPipeDropAction
 
 
 func _on_inventory_drop(_action_name: String, inventory_id: String, inventory_data: Dictionary) -> void:
-	Inventory.add_item("Pipe_Functional", { "preview": "res://assets/texture/ElevatorMain/Handle.png" })
+	Inventory.add_item("Pipe_Functional", { "preview": "res://assets/texture/ElevatorMain/Inventory_Preview_Pipe.png" })
 	ActionRelay.trigger("Pipe_Functional", "set_visible", [false])
 	
