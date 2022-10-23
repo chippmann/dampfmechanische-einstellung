0f5e48f6ae9836a05507434bb5d6a148f20b2770
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-23 09:46:55 +0200

    Restore pipe_defect state

---
+++ objects/ElevatorMain/Wartungsschacht/Pipe_Defect/Pipe_Defect_Action.gd
@@ -0,0 +1,11 @@
+extends Reference
+class_name Pipe_Defect_Action
+
+
+func _execute(_interactable_object_instance: InteractableObject, _action_name: String) -> void:
+	pass
+
+
+func _restore_state(_interactable_object_instance: InteractableObject, _action_name: String) -> void:
+	if Inventory.has_object("Pipe_Defect"):
+		ActionRelay.trigger("Pipe_Defect", "set_visible", [false])
--- objects/ElevatorMain/Wartungsschacht/Pipe_Defect/Pipe_Defect_InventoryDropCondition.gd
+++ objects/ElevatorMain/Wartungsschacht/Pipe_Defect/Pipe_Defect_InventoryDropCondition.gd
@@ -3,4 +3,4 @@ class_name Pipe_Defect_CanInventoryDropCondition
 
 
 func _can_handle_inventory_drop(_action_name: String, inventory_id: String, inventory_data: Dictionary) -> bool:
-	return inventory_id == "Wrench"
+	return inventory_id == "Wrench" && !Inventory.has_object("Pipe_Defect")
